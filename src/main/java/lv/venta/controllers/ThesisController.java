fa72dc8adefcaa19e353201d08e69bc03031e3a6
Everita Vecbērza <everita.vecberza@gmail.com>
2023-06-28 19:48:53 +0300

    Add: thesis/showAll  thesis/showAll/id  thesis/addNew (get)

--- src/main/java/lv/venta/controllers/ThesisController.java
+++ src/main/java/lv/venta/controllers/ThesisController.java
@@ -1,14 +1,20 @@
 package lv.venta.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lv.venta.models.Area;
+import lv.venta.models.Complexity;
 import lv.venta.models.Thesis;
+import lv.venta.models.users.AcademicPersonel;
+import lv.venta.repos.users.IAcademicPersonelRepo;
 import lv.venta.services.IThesisService;
 
 @Controller
@@ -17,6 +23,12 @@ public class ThesisController {
 
 	@Autowired
 	private IThesisService thesisService;
+	
+	@Autowired
+    private IAcademicPersonelRepo academicRepo;
+	
+	private ArrayList<Area> areas = new ArrayList<>(Arrays.asList(Area.values()));
+	private ArrayList<Complexity> complexities = new ArrayList<>(Arrays.asList(Complexity.values()));
 	
 	@GetMapping("/hello") 
 	public String helloFunc() {
@@ -28,7 +40,10 @@ public class ThesisController {
 	public String showAllTrips(Model model) {
 	    try {
 	        ArrayList<Thesis> thesis = thesisService.selectAllThesis();
-	        model.addAttribute("thesis", thesis);
+	        if (thesis.isEmpty()) {
+	            throw new Exception("No theses found!");
+	        }
+			model.addAttribute("thesis", thesis);
 			model.addAttribute("searchedElement", "All Thesis");
 	        return "thesis-all-page";
 	    } catch (Exception e) {
@@ -36,5 +51,32 @@ public class ThesisController {
 	        return "error-page";
 	    }
 	}
+	
+	@GetMapping("/showAll/{id}")
+	public String showDriverById(@PathVariable long id, Model model) {
+		try {
+	        ArrayList<Thesis> thesis = thesisService.selectAllThesisBySupervisor(id);
+	        if (thesis.isEmpty()) {
+	            throw new Exception("No theses found for Supervisor ID:" + id );
+	        }
+			model.addAttribute("thesis", thesis);
+			model.addAttribute("searchedElement", " Supervisor " + id);
+			return "thesis-all-page";
+		} catch (Exception e) {
+			model.addAttribute("error", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("/addNew")
+	public String showAddThesisForm(Model model) {
+		ArrayList<AcademicPersonel> supervisors = (ArrayList<AcademicPersonel>) academicRepo.findAll();
+        model.addAttribute("supervisors", supervisors);
+		model.addAttribute("thesis", new Thesis());
+		model.addAttribute("areas", areas);
+		model.addAttribute("complexities", complexities);
+		return "thesis-add-page";
+	}
 
+	
 }
